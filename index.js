a0ad1d2f89223985f63f07aad8b1c015f4d2ac0b
Sayantan <sp@unthread.world>
2025-01-20 00:35:31 +0530

    shit happens

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 import puppeteer from "puppeteer";
-// import { config } from "dotenv";
-// config();
+import { config } from "dotenv";
+config();
 
 (async () => {
   const browser = await puppeteer.launch({
-    // slowMo: 50,
+    slowMo: 50,
     // headless: false,
     defaultViewport: null,
     protocolTimeout: 6e4,
     args: ["--no-sandbox", "--start-maximized"],
     // userDataDir: "./.pupp",
-    // executablePath:
-    //   "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
+    // executablePath: "/usr/bin/chromium",
   });
 
   const context = browser.defaultBrowserContext();
-  await context.overridePermissions("https://tlconnect.teamlease.com", ["background-sync", "persistent-storage"]);
+  await context.overridePermissions("https://tlconnect.teamlease.com", [
+    "background-sync",
+    "persistent-storage",
+  ]);
 
   const page = await browser.newPage();
 
@@ -35,12 +37,24 @@ import puppeteer from "puppeteer";
 
   await page.waitForNavigation({ waitUntil: "load", timeout: 6e4 });
 
-  await page.waitForSelector("#btnchkin");
-  await page.click("#btnchkin");
+  // await page.waitForSelector("#btnchkin");
+  // await page.click("#btnchkin");
+  await page.waitForFunction(
+    (selector) => {
+      const btn = document.querySelector(selector);
+      // console.log(btn?.ariaDisabled, btn?.disabled, btn?.checkVisibility());
+      if (btn && btn.checkVisibility()) {
+        btn.click();
+        return true;
+      }
+    },
+    { polling: "raf" },
+    "#btnchkin"
+  );
 
-  await page.waitForSelector("button.confirm", { state: "visible" });
+  await page.waitForSelector("button.confirm");
   await page.click("button.confirm");
 
   await page.close();
   await browser.close();
-})();
+})().catch(console.log);
